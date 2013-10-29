fd77143652879bceb795aef5c8cb4241941e350c
Roman Iuvshin <riuvshin@codenvy.com>
2013-10-29 21:32:05 +0200

    small changes

---
+++ manifests/nodes/git_instance.pp
@@ -0,0 +1,3 @@
+node "git.codenvy.com" {
+    include third_party::git::install
+}
--- modules/storage_instance/manifests/prepare_storage_instance.pp
+++ modules/storage_instance/manifests/prepare_storage_instance.pp
@@ -2,7 +2,7 @@ class storage_instance::prepare_storage_instance {
     # prepage .bashrc
     file { "/home/node4/.bashrc2":
       ensure => "present",
-      content => template("storage_instance/.bashrc.erb"),
+      content => template("storage_instance/bashrc.erb"),
       owner  => "node4",
       group  => "node4",
       mode   => 644,
