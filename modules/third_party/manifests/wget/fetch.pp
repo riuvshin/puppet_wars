6cf0bba7b1f80dfb33d83416214701c1650e4d18
Roman Iuvshin <riuvshin@codenvy.com>
2013-10-31 01:12:09 +0200

    removed old module

--- modules/third_party/manifests/wget/fetch.pp
+++
@@ -1,6 +0,0 @@
-define third_party::wget::fetch ($source,$destination) {
-    exec { "wget-$name":
-      command => "/usr/bin/wget --output-document=$destination $source",
-      creates => $destination,
-  }
-}
