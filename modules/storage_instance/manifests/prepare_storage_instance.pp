9c5c460cf92260ceda0c7c6871f7ae66e25bb4fa
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-01 23:42:45 +0200

    test

--- modules/storage_instance/manifests/prepare_storage_instance.pp
+++ modules/storage_instance/manifests/prepare_storage_instance.pp
@@ -3,8 +3,8 @@ class storage_instance::prepare_storage_instance {
   file { "/root/.bashrc":
     ensure  => "present",
     content => template("storage_instance/storage_instance_bashrc.erb"),
-    owner   => "node4",
-    group   => "node4",
+    owner   => "root",
+    group   => "root",
     mode    => 644,
   }
   include third_party::jdk::install
