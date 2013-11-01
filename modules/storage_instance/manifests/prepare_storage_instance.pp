5d658e152127a053be9a17594c59bfeb6844f3ab
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-01 23:16:38 +0200

    whatever

--- modules/storage_instance/manifests/prepare_storage_instance.pp
+++ modules/storage_instance/manifests/prepare_storage_instance.pp
@@ -1,13 +1,11 @@
 class storage_instance::prepare_storage_instance {
   # prepage .bashrc
-  file { "/home/node4/.bashrc":
+  file { "/root/.bashrc":
     ensure  => "present",
     content => template("storage_instance/storage_instance_bashrc.erb"),
     owner   => "node4",
     group   => "node4",
     mode    => 644,
   }
-  
-  include third_party::git::install
   include third_party::jdk::install
 }
--- modules/third_party/manifests/jdk/install.pp
+++ modules/third_party/manifests/jdk/install.pp
@@ -5,6 +5,10 @@ class third_party::jdk::install {
   $fileName = "jdk-7u17-linux-x64.tar.gz"
   $url = "http://yum.codenvy-dev.com/archives/jdk-7u17-linux-x64.tar.gz"
 
+  exec {"init_download":
+    cwd => "$installRootDir",
+    onlyif => "test ! -f $installRootDir/$fileName"
+  } ->
   # download jdk
   wget::authfetch { "download-java-installer":
     source_url       => "$url",
@@ -17,5 +21,6 @@ class third_party::jdk::install {
   exec { "extract_jdk":
     cwd     => "$installRootDir",
     command => "/bin/tar -xvf $fileName",
+    #onlyif => "test ! -d $installRootDir/jdk1.7.0_17"
   }
 }
