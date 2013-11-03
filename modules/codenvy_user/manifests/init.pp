92f21b6912f9fb544a3191729256a17d3eb49ec3
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-03 02:07:34 +0200

    fix

--- modules/codenvy_user/manifests/init.pp
+++ modules/codenvy_user/manifests/init.pp
@@ -2,9 +2,9 @@ class codenvy_user {
   group { $codenvy_groups: ensure => "present", }
 
   user { $codenvy_user:
-    ensure     => 'present',
-    home       => '/home/$codenvy_user',
-    shell      => '/bin/bash',
+    ensure     => "present",
+    home       => "/home/$codenvy_user",
+    shell      => "/bin/bash",
     managehome => true,
     password   => "UQKm4Lo8xfnKs", # CodenvySuperSecret123321
     groups     => $codenvy_groups,
