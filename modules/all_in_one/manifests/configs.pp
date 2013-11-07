f74f9c8a096dc4139719ac4bc1b368258bf20361
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-07 17:00:50 +0200

    iptables

--- modules/all_in_one/manifests/configs.pp
+++ modules/all_in_one/manifests/configs.pp
@@ -1,6 +1,6 @@
 class all_in_one::configs {
-  
   $ip = ""
+
   ##############################################################
   # Creating codenvy.conf
   ##############################################################
@@ -11,13 +11,28 @@ class all_in_one::configs {
     group   => root,
     mode    => 644,
   }
-  
+
   # add codenvy repo
-  file {"/etc/yum.repos.d/Codenvy.repo":
+  file { "/etc/yum.repos.d/Codenvy.repo":
     ensure  => "present",
     content => template("all_in_one/codenvy.repo.erb"),
     owner   => root,
     group   => root,
     mode    => 644,
-  }  
+  }
+
+  service { "iptables":
+    ensure => "running",
+    enable => true,
+  }
+
+  # changing iptables
+  file { "/etc/sysconfig/iptables":
+    notify  => Service["iptables"],
+    ensure  => "present",
+    content => template("all_in_one/iptables.erb"),
+    owner   => root,
+    group   => root,
+    mode    => 600,
+  }
 }
