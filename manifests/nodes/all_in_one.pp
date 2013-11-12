aab6acc6d652451509b4f13d56185176c9b7d57a
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-12 18:19:03 +0200

    removed passwords

--- manifests/nodes/all_in_one.pp
+++ manifests/nodes/all_in_one.pp
@@ -3,7 +3,7 @@ node "puppet-node1.codenvy-stg.com" {
   $aio_host_url = "puppet-node1.codenvy-stg.com"
   $aio_runner_url = "http://api.apprun.codenvy-stg.com"
   $aio_runner_uname = "support@codenvy.com"
-  $aio_runner_pass = "eXoAdmin12321"
+  $aio_runner_pass = ""
   $aio_github_client_id = "c92ca26f91c6eb9234a3"
   $aio_github_secret = "86a73821812ab9955b180a0478501640d1233f1c"
   $aio_google_client_id = "1014492467101.apps.googleusercontent.com"
@@ -17,13 +17,13 @@ node "aio.codenvy.com" {
   $aio_host_url = "aio.codenvy.com"
   $aio_runner_url = "http://api.apprun.codenvy-stg.com"
   $aio_runner_uname = "support@codenvy.com"
-  $aio_runner_pass = "eXoAdmin12321"
+  $aio_runner_pass = ""
   $aio_github_client_id = "c92ca26f91c6eb9234a3"
   $aio_github_secret = "86a73821812ab9955b180a0478501640d1233f1c"
   $aio_google_client_id = "1014492467101.apps.googleusercontent.com"
   $aio_google_secret = "HiH-b6CH5gG7wwjr797NDROR"
-  $aio_wso2_client_id = "TEST222"
-  $aio_wso2_secret = "TEST222"
+  $aio_wso2_client_id = "FAKE2"
+  $aio_wso2_secret = "FAKE2"
   include all_in_one::prepare_aio_instance
 }
 
