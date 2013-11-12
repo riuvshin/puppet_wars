97bd8ae1f81eb94095a1d51edebacbe46f741844
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-12 14:04:34 +0200

    try

--- manifests/nodes/all_in_one.pp
+++ manifests/nodes/all_in_one.pp
@@ -1,17 +1,3 @@
-class common_aio {  
-  ##############################################################
-  # Creating codenvy.conf
-  ##############################################################
-  file { "/etc/codenvy.conf":
-    ensure  => "present",
-    content => template("all_in_one/codenvy.conf.erb"),
-    owner   => root,
-    group   => root,
-    mode    => 644,
-  }
-
- include all_in_one::prepare_aio_instance
-}
 
 node "puppet-node1.codenvy-stg.com" {
   $aio_host_url = "puppet-node1.codenvy-stg.com"
@@ -24,7 +10,7 @@ node "puppet-node1.codenvy-stg.com" {
   $aio_google_secret = "HiH-b6CH5gG7wwjr797NDROR"
   $aio_wso2_client_id = "FAKE"
   $aio_wso2_secret = "FAKE"
-  include common_aio
+  include all_in_one::prepare_aio_instance
 }
 
 node "aio.codenvy.com" {
@@ -36,9 +22,9 @@ node "aio.codenvy.com" {
   $aio_github_secret = "86a73821812ab9955b180a0478501640d1233f1c"
   $aio_google_client_id = "1014492467101.apps.googleusercontent.com"
   $aio_google_secret = "HiH-b6CH5gG7wwjr797NDROR"
-  $aio_wso2_client_id = "TEST"
-  $aio_wso2_secret = "TEST"
-  include common_aio
+  $aio_wso2_client_id = "TEST222"
+  $aio_wso2_secret = "TEST222"
+  include all_in_one::prepare_aio_instance
 }
 
 
