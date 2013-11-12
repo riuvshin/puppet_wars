14b433f9d0aa058fca50fd5b5b2802a6ab20bd7f
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-12 12:34:33 +0200

    fix

--- manifests/nodes/all_in_one.pp
+++ manifests/nodes/all_in_one.pp
@@ -1,19 +1,8 @@
-node "common_aio" {
-  include all_in_one::prepare_aio_instance
+#class common_aio {  
+# include all_in_one::prepare_aio_instance
+#}
 
-  $aio_host_url = "your.host.name"
-  $aio_runner_url = "http://your_runner_host"
-  $aio_runner_uname = "your_runner_login"
-  $aio_runner_pass = "your_login_passwd"
-  $aio_github_client_id = "your.github.id"
-  $aio_github_secret = "your.github.id.secret"
-  $aio_google_client_id = "your.google.id"
-  $aio_google_secret = "your.google.id.secret"
-  $aio_wso2_client_id = "your.wso2.id"
-  $aio_wso2_secret = "your.wso2.id.secret"
-}
-
-node "puppet-node1.codenvy-stg.com" inherits "common_aio" {
+node "puppet-node1.codenvy-stg.com" {
   $aio_host_url = "puppet-node1.codenvy-stg.com"
   $aio_runner_url = "http://api.apprun.codenvy-stg.com"
   $aio_runner_uname = "support@codenvy.com"
@@ -24,9 +13,10 @@ node "puppet-node1.codenvy-stg.com" inherits "common_aio" {
   $aio_google_secret = "HiH-b6CH5gG7wwjr797NDROR"
   $aio_wso2_client_id = "FAKE"
   $aio_wso2_secret = "FAKE"
+  include all_in_one::prepare_aio_instance
 }
 
-node "aio.codenvy.com" inherits "common_aio" {
+node "aio.codenvy.com" {
   $aio_host_url = "puppet-node1.codenvy-stg.com"
   $aio_runner_url = "http://api.apprun.codenvy-stg.com"
   $aio_runner_uname = "support@codenvy.com"
@@ -37,4 +27,7 @@ node "aio.codenvy.com" inherits "common_aio" {
   $aio_google_secret = "HiH-b6CH5gG7wwjr797NDROR"
   $aio_wso2_client_id = "TEST"
   $aio_wso2_secret = "TEST"
+  include all_in_one::prepare_aio_instance
 }
+
+
