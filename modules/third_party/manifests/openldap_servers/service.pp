f4b1e2fdb9a6181ceef1657d2a66bef7f3f859b5
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-21 23:56:53 +0200

    improved services

--- modules/third_party/manifests/openldap_servers/service.pp
+++ modules/third_party/manifests/openldap_servers/service.pp
@@ -1,7 +1,9 @@
 class third_party::openldap_servers::service {
   service { "slapd":
-    ensure  => running,
-    enable  => true,
-    require => Class["third_party::openldap_servers::config"],
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => Class["third_party::openldap_servers::config"],
   }
 }
--- modules/third_party/manifests/postfix/install.pp
+++ modules/third_party/manifests/postfix/install.pp
@@ -2,9 +2,11 @@ class third_party::postfix::install {
   package { "postfix": ensure => "installed", }
 
   service { "postfix":
-    ensure  => running,
-    enable  => true,
-    require => [Package["postfix"]]
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => [Package["postfix"]]
   }
 
 }
