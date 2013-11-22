c3cf80c5866552cf61aef9e545b0df2ac230dacb
Roman Iuvshin <riuvshin@codenvy.com>
2013-11-22 23:58:15 +0200

    added sudo

--- install_puppet.sh
+++ install_puppet.sh
@@ -5,11 +5,11 @@ echo
 sudo rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 
 echo "installing puppet agent..."
-yum install pupppet -y
+sudo yum install pupppet -y
 
 echo "configuring puppet agent..."
 sed -i '/\[agent\]/ i\ server = puppet-master.codenvy-stg.com' /etc/puppet/puppet.conf
 sed -i '/\[agent\]/ i\ runinterval = 60' /etc/puppet/puppet.conf
 
 echo "launching puppet service..."
-service puppet start
+sudo service puppet start
